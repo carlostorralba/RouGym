af79a27eeba07cd2ec2566f5438e6d283bdccbad
carlostorralba12 <carlostorralba12@gmail.com>
2021-10-24 19:41:51 +0200

    Close #10 Se añade estructura para entidad Rutina

--- src/rutina/rutina.go
+++ src/rutina/rutina.go
@@ -1,12 +1,19 @@
 package rutina
 
-type Tipo int
+type TipoRutina int
 
 const (
-	Definicion_Musculo Tipo = iota
+	Definicion_Musculo TipoRutina = iota
 	Ganar_Musculo
 	Bajar_peso
 	Resistencia_fisica
 
 )
 
+type Rutina struct {
+	Nombre string
+	Tipo TipoRutina
+	Dias_Entrenamiento int
+	Entrenamientos []Entrenamiento
+	Duracion int
+}
