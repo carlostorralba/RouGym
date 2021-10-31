7738b0b86b6ea58d91e4c71dae29653bfabd5287
carlostorralba <carlostorralba@correo.ugr.es>
2021-10-31 18:05:36 +0100

    Eliminación de comentarios

--- src/main.go
+++ src/main.go
@@ -13,37 +13,6 @@ func main() {
 	fmt.Printf("-------------RouGym------------\n")
 	fmt.Printf("-------------------------------\n")
 
-	/* 	ejercicio := rutina.Ejercicio{
-	   		Nombre:             "Remo con barra",
-	   		Series:             4,
-	   		Repeticiones:       12,
-	   		DescansoRepeticion: 90,
-	   	}
-
-	   	entrenamiento := rutina.Entrenamiento{
-	   		Nombre:     "Espalda",
-	   		Ejercicios: []rutina.Ejercicio{},
-	   	}
-
-	   	entrenamiento.Ejercicios = append(entrenamiento.Ejercicios, ejercicio)
-
-	   	fmt.Println(entrenamiento.Ejercicios[0].Nombre)
-
-	   	tipo := rutina.Ganar_Musculo
-
-	   	rutina1 := rutina.Rutina{
-	   		Nombre:             "Rutina 1",
-	   		Dias_Entrenamiento: 4,
-	   		Tipo:               tipo,
-	   		Entrenamientos:     []rutina.Entrenamiento{},
-	   		Duracion:           6,
-	   	}
-
-	   	rutina1.Entrenamientos = append(rutina1.Entrenamientos, entrenamiento)
-
-	   	fmt.Println(rutina1.Entrenamientos[0].Nombre)
-	   	fmt.Println(rutina1.Tipo)
-	*/
 	router := mux.NewRouter()
 
 	router.HandleFunc("/rutinas", rutina.AddRutina).Methods("POST")
