c4e0931ff229853ff8acc45d8a728307e091377f
carlostorralba <carlostorralba@correo.ugr.es>
2021-10-31 11:02:36 +0100

    Close #31 Añadidos métodos para la creación y almacenamiento de rutinas

--- src/rutina/rutina.go
+++ src/rutina/rutina.go
@@ -1,5 +1,11 @@
 package rutina
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
 type TipoRutina int
 
 const (
@@ -7,13 +13,41 @@ const (
 	Ganar_Musculo
 	Bajar_peso
 	Resistencia_fisica
-
 )
 
 type Rutina struct {
-	Nombre string
-	Tipo TipoRutina
+	Nombre             string
+	Tipo               TipoRutina
 	Dias_Entrenamiento int
-	Entrenamientos []Entrenamiento
-	Duracion int
+	Entrenamientos     []Entrenamiento
+	Duracion           int
+}
+
+var rutinas []Rutina = []Rutina{}
+
+func AddRutina(w http.ResponseWriter, r *http.Request) {
+
+	var rutina Rutina
+	json.NewDecoder(r.Body).Decode(&rutina)
+
+	rutinas = append(rutinas, rutina)
+
+	almacenarRutinaJson()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode("Rutina almacenada correctamente")
+
+}
+
+func almacenarRutinaJson() {
+	jsonRutinas, err := json.Marshal(rutinas)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("rutinas.json", jsonRutinas, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
